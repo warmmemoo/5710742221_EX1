0cb810468b2937e1d9d38c1a77566074d2d56510
warmmemoo <warmmemoo@gmail.com>
2014-09-03 15:31:06 +0700

    ข้อ3

---
+++ 5710742221_EX.3/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+
+    printf("Enter X = ",x);
+    scanf("%d",&x);
+    printf("Enter Y = ",y);
+    scanf("%d",&y);
+    printf("X*Y = %d\n",x*y);
+    printf("X/Y = %d\n",x/y);
+    return 0;
+}
+
+
