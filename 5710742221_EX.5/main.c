5ba24a19318b63ce86411518fc30972a33f6cb2f
warmmemoo <warmmemoo@gmail.com>
2014-09-03 15:31:30 +0700

    ข้อ5

---
+++ 5710742221_EX.5/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void main()
+{
+    char B;
+    printf("Enter the character:");
+    scanf("%c",&B);
+    printf("Lowercase charactor:%c\n",tolower(B));
+    return 0;
+
+}
