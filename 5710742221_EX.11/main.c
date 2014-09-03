6a6f4932d9beb5a210e60992f3b381a19706c62a
warmmemoo <warmmemoo@gmail.com>
2014-09-03 15:32:37 +0700

    ข้อ11

---
+++ 5710742221_EX.11/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int D;
+    printf("The Decimal Number : ");
+    scanf("%d",&D);
+    printf("The Octal Number : %o\n",D);
+    printf("The Hexadecimal Number : %x",D);
+    return 0;
+}
